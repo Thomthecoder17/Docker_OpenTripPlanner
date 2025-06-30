1bddc68dafb1865dda738dc14ea5cb6e5c7257e1
Thomthecoder17 <thombitrob17@gmail.com>
2025-06-30 17:15:18 -0400

    Removed Google Cloud Function Code

--- write_build-config/build-config-updater.py
+++ write_build-config/build-config-updater.py
@@ -1,16 +1,19 @@
 import os, json
 
+# Creates the lists of files to use in build-config.json
 gtfsFiles = [
     f"file:///otp-data/gtfs/{file}"
     for file in os.listdir("/otp-data/gtfs")
 ]
-
 osmFiles = [
     f"file:///otp-data/{file}"
     for file in os.listdir("/otp-data/osm")
 ]
 
+# Compiles the data to write to build-config.json
 buildConfig = {
+    # ADD ANY OTHER FIELDS FOR build-config.json HERE
+
     "transitModelTimeZone": "America/New_York",
     "storage": {
         "gtfs": gtfsFiles,
@@ -20,5 +23,6 @@ buildConfig = {
     }
 }
 
+# Overwrites build-config.json with updated data
 with open("/otp-data/build-config.json", "w") as file:
     json.dump(buildConfig, file, indent=2)
